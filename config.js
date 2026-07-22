8494d2fbe120d2b791d9660d530859cc0cadc840
karthiktanneru5 <karthiktanneru5@gmail.com>
2026-07-21 22:48:46 -0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Google OAuth Web Client ID from Google Cloud Console.
-// Client IDs are identifiers, not passwords/secrets.
-window.FAMILY_FINANCE_CONFIG = {
-  googleClientId: "PASTE_GOOGLE_OAUTH_WEB_CLIENT_ID_HERE.apps.googleusercontent.com"
-};
